13f09dba456570496133a06b25c4ac6cc131930c
sdushantha <siddharth.dushantha@gmail.com>
2019-05-25 20:33:15 +0200

    minor fix to setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/sdushantha/instadp',
-    py_modules=['instadp']
+    py_modules=['instadp'],
     scripts=['instadp/instadp'],
     install_requires=['requests']
 )
